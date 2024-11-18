c69c54bc0114637a2e8c554084009d724817f658
Ghulam Mustafa <mustafaji@gmail.com>
2024-11-18 02:06:14 +0100

    worked on promptai app

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,43 @@
-import React from '@styles/globals.css';
+import "@styles/globals.css";
 
-const layout = () => {
+import Nav from "@components/Nav"; 
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover and Share AI Prompts",
+  openGraph: {
+    title: "Promptopia",
+    description: "Discover and Share AI Prompts",
+    images: [
+      {
+        url: "https://i.ibb.co/mtRSg6p/Screenshot-233.png",
+      },
+    ],
+  },
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
+};
+
+const RootLayout = ({ children }) => {
+  // console.log(JSON.stringify(children));
   return (
-    <div>layout</div>
-  )
-}
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-export default layout
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,20 @@
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
-    <div>page</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptai is an open-source AI prompting tool for moden world to
+        discover, create and share creative prompts
+      </p>
 
-export default Home
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
